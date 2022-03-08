890eed6572cbc137c2400a52fb7f03e6b3fb7c9a
amin-abir <amin@jouleslabs.com>
2022-03-08 18:03:55 +0600

    design change

--- inc/widgets/social-icons.php
+++ inc/widgets/social-icons.php
@@ -5,13 +5,13 @@
 // Social icons widget
 //---------------------------------------------------------------------------
 
-class roza_social_Widget extends WP_Widget {
+class minaz_social_Widget extends WP_Widget {
 
 	public function __construct() {
 		parent::__construct(
-			'roza_social_button', // Base ID
-			__( 'Roza Social Icons', 'roza' ), // Name
-			array( 'description' => __( 'Displaying social icons', 'roza' ), ) // Args
+			'minaz_social_button', // Base ID
+			__( 'Minaz Social Icons', 'minaz' ), // Name
+			array( 'description' => __( 'Displaying social icons', 'minaz' ), ) // Args
 		);
 	}
 
@@ -20,7 +20,6 @@ class roza_social_Widget extends WP_Widget {
 		$title     = isset( $instance['title'] ) ? $instance['title'] : '';
 		$facebook  = ( isset( $instance['facebook'] ) ) ? $instance['facebook'] : '';
 		$twitter   = ( isset( $instance['twitter'] ) ) ? $instance['twitter'] : '';
-		$google    = ( isset( $instance['google'] ) ) ? $instance['google'] : '';
 		$pinterest = ( isset( $instance['pinterest'] ) ) ? $instance['pinterest'] : '';
 		$dribbble  = ( isset( $instance['dribbble'] ) ) ? $instance['dribbble'] : '';
 		$youtube   = ( isset( $instance['youtube'] ) ) ? $instance['youtube'] : '';
@@ -28,7 +27,7 @@ class roza_social_Widget extends WP_Widget {
 		?>
 
 		<p>
-			<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_html_e( 'Title :', 'roza' );
+			<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_html_e( 'Title :', 'minaz' );
 				?></label>
 			<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"
 			       name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text"
@@ -36,7 +35,7 @@ class roza_social_Widget extends WP_Widget {
 		</p>
 		<p>
 			<label
-				for="<?php echo esc_attr( $this->get_field_id( 'facebook' ) ); ?>"><?php esc_html_e( 'Enter facebook URL:', 'roza' );
+				for="<?php echo esc_attr( $this->get_field_id( 'facebook' ) ); ?>"><?php esc_html_e( 'Enter facebook URL:', 'minaz' );
 				?></label>
 			<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'facebook' ) ); ?>"
 			       name="<?php echo esc_attr( $this->get_field_name( 'facebook' ) ); ?>" type="text"
@@ -44,28 +43,22 @@ class roza_social_Widget extends WP_Widget {
 		</p>
 		<p>
 			<label
-				for="<?php echo esc_attr( $this->get_field_id( 'twitter' ) ); ?>"><?php esc_html_e( 'Enter twitter URL:', 'roza' ); ?></label>
+				for="<?php echo esc_attr( $this->get_field_id( 'twitter' ) ); ?>"><?php esc_html_e( 'Enter twitter URL:', 'minaz' ); ?></label>
 			<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'twitter' ) ); ?>"
 			       name="<?php echo esc_attr( $this->get_field_name( 'twitter' ) ); ?>" type="text"
 			       value="<?php echo esc_url( $twitter ); ?>">
 		</p>
+		
 		<p>
 			<label
-				for="<?php echo esc_attr( $this->get_field_id( 'google' ) ); ?>"><?php esc_html_e( 'Enter google URL:', 'roza' ); ?></label>
-			<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'google' ) ); ?>"
-			       name="<?php echo esc_attr( $this->get_field_name( 'google' ) ); ?>" type="text"
-			       value="<?php echo esc_url( $google ); ?>">
-		</p>
-		<p>
-			<label
-				for="<?php echo esc_attr( $this->get_field_id( 'pinterest' ) ); ?>"><?php esc_html_e( 'Enter pinterest URL:', 'roza' ); ?></label>
+				for="<?php echo esc_attr( $this->get_field_id( 'pinterest' ) ); ?>"><?php esc_html_e( 'Enter pinterest URL:', 'minaz' ); ?></label>
 			<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'pinterest' ) ); ?>"
 			       name="<?php echo esc_attr( $this->get_field_name( 'pinterest' ) ); ?>" type="text"
 			       value="<?php echo esc_url( $pinterest ); ?>">
 		</p>
 		<p>
 			<label
-				for="<?php echo esc_attr( $this->get_field_id( 'dribbble' ) ); ?>"><?php esc_html_e( 'Enter dribbble URL:', 'roza' ); ?></label>
+				for="<?php echo esc_attr( $this->get_field_id( 'dribbble' ) ); ?>"><?php esc_html_e( 'Enter dribbble URL:', 'minaz' ); ?></label>
 			<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'dribbble' ) ); ?>"
 			       name="<?php echo esc_attr( $this->get_field_name( 'dribbble' ) ); ?>" type="text"
 			       value="<?php echo esc_url( $dribbble ); ?>">
@@ -73,7 +66,7 @@ class roza_social_Widget extends WP_Widget {
 		
 		<p>
 			<label
-				for="<?php echo esc_attr( $this->get_field_id( 'youtube' ) ); ?>"><?php esc_html_e( 'Enter youtube URL:', 'roza' ); ?></label>
+				for="<?php echo esc_attr( $this->get_field_id( 'youtube' ) ); ?>"><?php esc_html_e( 'Enter youtube URL:', 'minaz' ); ?></label>
 			<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'youtube' ) ); ?>"
 			       name="<?php echo esc_attr( $this->get_field_name( 'youtube' ) ); ?>" type="text"
 			       value="<?php echo esc_url( $youtube ); ?>">
@@ -87,7 +80,6 @@ class roza_social_Widget extends WP_Widget {
 		$instance['title']     = sanitize_text_field( $new_instance['title'] );
 		$instance['facebook']  = ( ! empty( $new_instance['facebook'] ) ) ? esc_url_raw( $new_instance['facebook'] ) : '';
 		$instance['twitter']   = ( ! empty( $new_instance['twitter'] ) ) ? esc_url_raw( $new_instance['twitter'] ) : '';
-		$instance['google']    = ( ! empty( $new_instance['google'] ) ) ? esc_url_raw( $new_instance['google'] ) : '';
 		$instance['pinterest'] = ( ! empty( $new_instance['pinterest'] ) ) ? esc_url_raw( $new_instance['pinterest'] ) : '';
 		$instance['dribbble']  = ( ! empty( $new_instance['dribbble'] ) ) ? esc_url_raw( $new_instance['dribbble'] ) : '';
 		$instance['youtube']   = ( ! empty( $new_instance['youtube'] ) ) ? esc_url_raw( $new_instance['youtube'] ) : '';
@@ -96,7 +88,7 @@ class roza_social_Widget extends WP_Widget {
 	}
 
 	public function widget( $args, $instance ) {
-		$title = apply_filters( 'widget_title', empty( $instance['title'] ) ? __( 'Social Icons', 'roza' ) :
+		$title = apply_filters( 'widget_title', empty( $instance['title'] ) ? __( 'Social Icons', 'minaz' ) :
 			$instance['title'], $instance, $this->id_base );
 
 		echo $args['before_widget'];
@@ -105,7 +97,7 @@ class roza_social_Widget extends WP_Widget {
 		}
 		?>
 
-		<div class="roza-social-link">
+		<div class="minaz-social-link">
 			<ul class="list-inline">
 				<?php $facebook_link = isset($instance['facebook']) ? $instance['facebook'] : '';
 				if ( $facebook_link ) { ?>
@@ -115,11 +107,6 @@ class roza_social_Widget extends WP_Widget {
 				<?php $twitter_link = isset($instance['twitter']) ? $instance['twitter'] : '';
 				if ( $twitter_link ) { ?>
 					<li><a href="<?php echo esc_url( $twitter_link )?>"class="twitter"><i class="fa fa-twitter"></i></a></li>
-				<?php } ?>
-
-				<?php $google_link = isset($instance['google']) ? $instance['google'] : '';
-				if ( $google_link ) { ?>
-					<li><a href="<?php echo esc_url( $google_link )?>"class="google-plus"><i class="fa fa-google-plus"></i></a></li>
 				<?php } ?>
 
 
@@ -145,11 +132,11 @@ class roza_social_Widget extends WP_Widget {
 
 
 // register widgets
-if ( ! function_exists( 'roza_social_icon_register' ) ) {
-	function roza_social_icon_register() {
-		register_widget( 'roza_social_Widget' );
+if ( ! function_exists( 'minaz_social_icon_register' ) ) {
+	function minaz_social_icon_register() {
+		register_widget( 'minaz_social_Widget' );
 	}
 
-	add_action( 'widgets_init', 'roza_social_icon_register' );
+	add_action( 'widgets_init', 'minaz_social_icon_register' );
 }
 
